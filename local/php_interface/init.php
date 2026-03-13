901b6de1a8b3e1b88ed132d6aee325c8abb1847e
v.pechatnov <v.pechatnov@zaryad.team>
2026-03-13 10:35:49 +0300

    Подключение автозагрузчика

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,1 +1,8 @@
 <?php
+
+// Подключаем автозагрузчик Composer (Monolog и другие зависимости)
+$composerAutoload = $_SERVER['DOCUMENT_ROOT'] . '/local/vendor/autoload.php';
+
+if (file_exists($composerAutoload)) {
+    require_once $composerAutoload;
+}
